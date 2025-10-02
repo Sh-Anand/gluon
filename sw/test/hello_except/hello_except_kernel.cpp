e5cc7737299ee6f8993c5f22ca6f3613ae98bba6
Shashank Anand <shashank.anand@berkeley.edu>
2025-10-02 16:37:31 -0700

    create program that throws exception, without being optimized away by the compiler

--- sw/test/hello_except/hello_except_kernel.cpp
+++ sw/test/hello_except/hello_except_kernel.cpp
@@ -1,3 +1,5 @@
-extern "C" __attribute__((noreturn)) void hello_except_kernel(int code) {
-    throw code;
+extern "C" int hello_except_kernel(int code) {
+    volatile int zero = code - code;
+    int crash = code / zero;
+    return crash;
 }
