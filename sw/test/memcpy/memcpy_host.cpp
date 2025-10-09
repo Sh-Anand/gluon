065cc8b3c5f90b4b899b8b124a740dcae7b25a50
Shashank Anand <shashank.anand@berkeley.edu>
2025-10-08 23:18:35 -0700

    test: update memcpy test

--- sw/test/memcpy/memcpy_host.cpp
+++ sw/test/memcpy/memcpy_host.cpp
@@ -4,18 +4,31 @@
 
 int main() {
     printf("Performing memcpy\n");
-    radParamBuf params;
+    
     size_t bytes = 1024;
+    
     void *dst;
     radMalloc(&dst, bytes);
     void *src = malloc(bytes);
     memset(src, 5, bytes);
     radMemCpy(dst, src, bytes, radMemCpyDir_H2D);
     free(src);
+
     radError err;
     radGetError(&err);
     printf("Error: %d\n", err.err_code);
     printf("Command ID: %d\n", err.cmd_id);
-    printf("PC: %d\n", err.pc);
+    
+    void *ptr = malloc(bytes);
+    radMemCpy(ptr, dst, bytes, radMemCpyDir_D2H);
+    radGetError(&err);
+    printf("Error: %d\n", err.err_code);
+    printf("Command ID: %d\n", err.cmd_id);
+
+    for (size_t i = 0; i < bytes; i++) {
+        printf("%d ", ((char *)ptr)[i]);
+    }
+
+    free(ptr);
     return 0;
 }
