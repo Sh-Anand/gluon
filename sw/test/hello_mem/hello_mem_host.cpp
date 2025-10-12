6103e2c5b04e34d2e051f5d547cd62ea65aa44a6
Shashank Anand <shashank.anand@berkeley.edu>
2025-10-12 16:07:37 -0700

    test: update hello_mem test

--- sw/test/hello_mem/hello_mem_host.cpp
+++ sw/test/hello_mem/hello_mem_host.cpp
@@ -4,8 +4,8 @@
 int main() {
     printf("Allocating GPU memory\n");
     int x[2] = {5, 6};
-    void *x_ptr;
-    radMalloc(&x_ptr, 8);
+    void* x_ptr;
+    radMalloc(&x_ptr, 12);
     printf("Copying to GPU memory\n");
     radMemCpy(x_ptr, x, 8, radMemCpyDir_H2D);
     radError err;
@@ -17,17 +17,23 @@ int main() {
     radDim3 grid = {1, 1, 1};
     radDim3 block = {1, 1, 2};
     radParamBuf params;
-    uint32_t x_ptr_u32 = static_cast<uint32_t>(reinterpret_cast<uintptr_t>(x_ptr));
-    uint32_t y_ptr_u32 = x_ptr_u32 + 4;
-
-    printf("Pushing device pointers 0x%x and 0x%x\n", x_ptr_u32, y_ptr_u32);
-    
-    params.push(x_ptr_u32);
-    params.push(y_ptr_u32);
+    void* y_ptr = (char*) x_ptr + 4;
+    void* z_ptr = (char*) x_ptr + 8;
+    printf("Pushing device pointers 0x%p and 0x%p and 0x%p\n", (uint8_t*)x_ptr, (uint8_t*)y_ptr, (uint8_t*)z_ptr); 
+    params.push(x_ptr);
+    params.push(y_ptr);
+    params.push(z_ptr);
     radKernelLaunch("hello_mem_kernel", grid, block, &params);
     radGetError(&err);
     printf("Error: %d\n", err.err_code);
     printf("Command ID: %d\n", err.cmd_id);
     printf("PC: 0x%x\n", err.pc);
+
+    int z;
+    radMemCpy(&z, z_ptr, 4, radMemCpyDir_D2H);
+    radGetError(&err);
+    printf("Error: %d\n", err.err_code);
+    printf("Command ID: %d\n", err.cmd_id);
+    printf("Host received: %d\n", z);
     return 0;
 }
--- sw/test/hello_mem/hello_mem_kernel.cpp
+++ sw/test/hello_mem/hello_mem_kernel.cpp
@@ -1,11 +1,10 @@
 #include <rad_print.h>
 #include <rad_defs.h>
 
-extern "C" int hello_mem_kernel(int *x, int *y) {
+extern "C" void hello_mem_kernel(int *x, int *y, int *z) {
     rad_printf("Hello, World\n");
     rad_printf("Pointers x: %p, y: %p\n", x, y);
     rad_printf("Inputs: %d, %d\n", *x, *y);
-    int z = *x + *y;
-    rad_printf("Result: %d\n", z);
-    return z;
+    *z = *x + *y;
+    rad_printf("Result: %d\n", *z);
 }
